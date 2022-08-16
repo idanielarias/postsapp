6707048b97d224e896cfacf51b441fc32c061cb7
idanielarias <ariasdaniel@hotmail.com>
2022-08-16 01:42:14 -0300

    initial files

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import Constants from "./utilities/Constants";
+import PostCreateForm from "./components/PostCreateForm";
+import PostUpdateForm from "./components/PostUpdateForm";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [showingCreateNewPostForm, setShowingCreateNewPostForm] = useState(false);
+  const [postCurrentlyBeingUpdated, setPostCurrentlyBeingUpdated] = useState(null);
+
+  function getPosts() {
+    const url = Constants.API_URL_GET_ALL_POSTS;
+    fetch(url, {
+      method: "GET",
+    }).then(response => response.json())
+      .then(postsFromServer => {
+        setPosts(postsFromServer);
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  function deletePost(postId){
+    const url = `${Constants.API_URL_DELETE_POST_BY_ID}/${postId}`;
+    fetch(url, {
+      method: "DELETE",
+    }).then(response => response.json())
+      .then(responseFromServer => {
+        console.log(responseFromServer);
+        onPostDeleted(postId);
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="container">
+      |<div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          {(showingCreateNewPostForm === false && postCurrentlyBeingUpdated === null) && (
+            <div>
+              <h1>Posts Dashboard</h1>
+              <div className="mt-5">
+                <button onClick={getPosts} className="btn btn-dark btn-lg w-100">Get Posts from server</button>
+                <button onClick={() => setShowingCreateNewPostForm(true)} className="btn btn-secondary btn-lg w-100 mt-4">Create New Post</button>
+              </div>
+            </div>
+          )}
+
+          {(posts.length > 0 && showingCreateNewPostForm === false && postCurrentlyBeingUpdated === null) && renderPostsTable()}
+
+          {showingCreateNewPostForm && <PostCreateForm onPostCreated={onPostCreated} />}
+
+          {postCurrentlyBeingUpdated !== null && <PostUpdateForm post={postCurrentlyBeingUpdated} onPostUpdated={onPostUpdated} />}
+        </div>
+      </div>
+    </div>
+  );
+
+  function renderPostsTable() {
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (PK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr key={post.postId}>
+                <th scope="row">{post.postId}</th>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>
+                  <button onClick={() => setPostCurrentlyBeingUpdated(post)} className="btn btn-dark btn-lg mx-3 my-3">Update</button>
+                  <button onClick={() => {if(window.confirm('Are you sure you want to Delete the post?')) deletePost(post.postId)}} className="btn btn-secondary btn-lg">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={() => setPosts([])} className="btn btn-dark btn-lg w-100">Empty</button>
+      </div>
+    )
+  }
+
+  function onPostCreated(createdPost) {
+    setShowingCreateNewPostForm(false);
+    if (createdPost === null) {
+      return;
+    }
+    alert(`Post created successfully! Title: "${createdPost.title}`);
+    getPosts();
+  }
+
+  function onPostUpdated(updatedPost) {
+    setPostCurrentlyBeingUpdated(null);
+    if (updatedPost === null) {
+      return;
+    }
+    let postsCopy = [...posts];
+    const index = postsCopy.findIndex((postsCopyPost, currentIndex) => {
+      if (postsCopyPost.postId === updatedPost.postId) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      postsCopy[index] = updatedPost;
+    }
+    setPosts(postsCopy);
+    alert(`Post updated successfully! Title: "${updatedPost.title}`);
+  }
+
+  function onPostDeleted(deletedPostId) {
+    let postsCopy = [...posts];
+    
+    const index = postsCopy.findIndex((postsCopyPost, currentIndex) => {
+      if (postsCopyPost.postId === deletedPostId) {
+        return true;
+      }
+    });
+
+    if (index !== -1) {
+      postsCopy.splice(index, 1);
+    }
+
+    setPosts(postsCopy);
+    alert("Post deleted successfully!");
+  }
+}
